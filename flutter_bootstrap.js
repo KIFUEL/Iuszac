4bf508ae142010a565890a1e18c090ed416bfdcb
KIFUEL <KIFUEL@users.noreply.github.com>
2026-06-08 20:42:26 +0000

    deploy: c37b13bb4b38ffe91c89b139e942cb2e69a5a3ec

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2042080400" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "414837681" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
